d883d43dcd0c807efd4c19d5ef6eff171be79b3c
artumax9 <arturojp18@gmail.com>
2024-07-13 14:53:22 -0300

    Add users index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
 
-  def show; end
+  def show
+  end
 
   def index
     @articles = Article.all
@@ -11,7 +12,8 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def edit; end
+  def edit
+  end
 
   def create
     @article = Article.new(article_params)
